bb0c468e12fa5e5df0a6747dc0e9c2ba9e00a018
bala227 <115527760+bala227@users.noreply.github.com>
2025-04-26 20:12:12 +0530

    final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../components/Footer.css'
 export default function Footer() {
   return (
     <footer className='footer'>
-      <p>🚀 Designed & Built by <strong>Bala Subramanian S</strong></p>
+      <p>Designed & Built by <strong>Bala Subramanian S</strong></p>
       <p>© {new Date().getFullYear()} | All Rights Reserved | <span>Made with ❤️ dedication & a spark of creativity ✨</span></p>
     </footer>
   )
